3be8e04ac5f82b566681df3a60476437871facf3
EngineerXL <8928632@gmail.com>
2020-10-05 12:32:39 +0300

    Обновил вторую лабу

--- lab2/child.c
+++ lab2/child.c
@@ -3,9 +3,31 @@
 #include <unistd.h>
 
 signed main() {
-	int x, y, z;
-	while (scanf("%d%d%d", &x, &y, &z) > 0) {
-		int res = x + y + z;
-		write(fileno(stdout), &res, sizeof(int));
+	char c;
+	int num = 0, sum = 0, minus = 0;
+	while (scanf("%c", &c) > 0) {
+		if (c == ' ' || c == '\t') {
+			if (minus) {
+				sum = sum - num;
+			} else {
+				sum = sum + num;
+			}
+			num = 0;
+			minus = 0;
+		} else if (c == '-') {
+			minus = 1;
+		} else if (c == '\n') {
+			if (minus) {
+				sum = sum - num;
+			} else {
+				sum = sum + num;
+			}
+			num = 0;
+			minus = 0;
+			write(STDOUT_FILENO, &sum, sizeof(int));
+			sum = 0;
+		} else if ('0' <= c && c <= '9') {
+			num = num * 10 + c - '0';
+		}
 	}
 }
--- lab2/main.c
+++ lab2/main.c
@@ -3,26 +3,46 @@
 #include <unistd.h>
 
 signed main() {
-	char* s = malloc(sizeof(char) * 64);
-	for (int i = 0; i < 64; i++) {
+	char* s = NULL;
+	s = malloc(sizeof(char) * 256);
+	if (s == NULL) {
+		printf("Error allocating memory!");
+		return -1;
+	}
+	for (int i = 0; i < 256; i++) {
 		s[i] = 0;
 	}
 	scanf("%s", s);
 	int fd[2];
-	pipe(fd);
+	if (pipe(fd) == -1) {
+		printf("Error creating pipe!");
+		return 1;
+	}
 	int id = fork();
 	if (id == -1) {
-		return -1;
+		printf("Error creating process!");
+		return 2;
 	} else if (id == 0) {
 		close(fd[0]);
 		// printf("[%d] is a child process\n", getpid());
-		freopen(s, "r", stdin);
-		dup2(fd[1], fileno(stdout));
-		execv("child.out", NULL);
+		FILE * input = NULL;
+		input = freopen(s, "r", stdin);
+		if (input == NULL) {
+			printf("Error opening file!\n");
+			return 3;
+		}
+		if (dup2(fd[1], STDOUT_FILENO) == -1) {
+			printf("Error changing stdout!\n");
+			return 4;
+		}
+		char * const * argv = NULL;
+		if (execv("child.out", argv) == -1) {
+			printf("Error executing child process!\n");
+			return 5;
+		}
 	} else {
 		close(fd[1]);
 		// printf("[%d] is a parent process\n", getpid());
-		// freopen("output.txt", "w", stdout);
 		int res;
 		while (read(fd[0], &res, sizeof(int)) > 0) {
 			printf("%d\n", res);
